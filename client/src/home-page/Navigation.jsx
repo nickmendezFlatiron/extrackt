c9bbcf86138fb0de7690aeec91a9fb6530b71bd0
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-09-06 22:23:19 -0400

    formatted navbar

--- client/src/home-page/Navigation.jsx
+++ client/src/home-page/Navigation.jsx
@@ -8,9 +8,8 @@ const Navigation = () => {
   return (
     <Navbar  variant="light" className="mx-5 fs-5 align-items-end">
           <Navbar.Brand href="/"><h2 className="text-dark light-purple-bg px-2 rounded-3 pt-3">Extrackt.</h2></Navbar.Brand>
-          <Nav className="me-auto pb-1">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/plans">Plans</Nav.Link>
+          <Nav className="me-auto pb-1 fs-4">
+            <Nav.Link href="/plans" className="mx-2">Plans</Nav.Link>
             <Nav.Link  href="/about">About</Nav.Link>
           </Nav>
           <Button variant="dark" className="fs-5 fw-bold text-white mb-2">Login</Button>
