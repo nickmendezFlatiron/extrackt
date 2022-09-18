4a73997ad91ea9846ec9e8cfda73acfd1af59757
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-09-18 18:37:05 -0400

    added custom validation for key attribute in sample model

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -4,10 +4,11 @@ class Collection < ApplicationRecord
   has_many :samples , dependent: :destroy
 
   # name , downloads , :user_id , :description , :cover_art
-
-  validates :name , uniqueness: { case_sensitive: false } ,format: {with: /[\.a-zA-z0-9]/ , message: "may only include alphanumeric characters and a ."}
+  validates_associated :samples
+  validates :name , uniqueness: { case_sensitive: false } ,format: {with: /[\.a-zA-z0-9\s]/ , message: "may only include alphanumeric characters, spaces, and a ."}
   validates :name , presence: true ,on: :create
-  validates :description , format: {with: /[\.a-zA-Z]/ , message: "may only include letters and a ."}
+  validates :description , format: {with: /[\.a-zA-Z\s]/ , message: "may only include letters, spaces, and a ."}
   validates :description , presence: true , on: :create
   validates :cover_art, presence: true, blob: { content_type: :image, size_range: 1..(5.megabytes)}
+  validates :user_id , numericality: true
 end
--- app/models/sample.rb
+++ app/models/sample.rb
@@ -3,7 +3,6 @@ class Sample < ApplicationRecord
   has_one_attached :audio_file , dependent: :destroy
   has_many :downloads , dependent: :destroy
 
-  # name , key , bpm , genre , type , collection_id , downloads , audio_file
   validates :name , presence: true , format: {with: /[a-zA-z0-9_-]/ , message: "may only include alphanumeric characters , underscores_ , dashes-"}
   validates :bpm , presence: true , numericality: { only_integer: true }
   validates :genre , presence: true , inclusion: {in: [ 
@@ -11,10 +10,28 @@ class Sample < ApplicationRecord
     "metal", "electronic", "soul", "ambient", "funk","raggae", 
     "disco","classical","house","indie","techno","trap","dubstep", 
     "gospel","latin", "raggaeton", "grime", "edm", "synthwave", "cinematic", 
-    "trance", "experimental","electro","idm","acapella"], message: "is not a valid genre" }
+    "trance", "experimental","electro","idm","acapella"], message: "is an ivalid genre, select a genre from the provided list" }
   validates :sample_type, presence: true ,inclusion: {
     in: ['drum', 'foley', 'fx', "loop", "melody", "one-shot", "pad", "percussion", "vocal"],
-    message: "is not a valid sample type"
+    message: "is not a valid sample type, select a sample type from the provided list"
   }
   validates :audio_file , presence: true, blob: { content_type: ['audio/x-wav']}
+  validates :collection_id, numericality: true
+  validate :key_validator
+
+  def key_validator
+    letter_count = key.select {|k| ["a", "b", "c", "d", "e", "f", "g"].include?(k)==true}
+    step_count = key.select {|k| ["sharp", "flat"].include?(k)==true}
+    scale_count = key.select {|k| ["major", "minor"].include?(k)==true}
+    
+    if letter_count.length > 1 
+      errors.add(:key, "invalid, only 1 letter allowed ")
+    end
+    if step_count.length > 1
+      errors.add(:key, "invalid, only 1 half step allowed")
+    end
+    if scale_count.length > 1
+      errors.add(:key, "invalid, only 1 scale allowed")
+    end
+  end
 end
