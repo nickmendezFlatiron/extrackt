419ae900fd94eab6982bf95f1a5a31976154b9ac
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-09-19 13:08:16 -0400

    added volume value label to audioplayer component

--- client/src/audio-player/AudioPlayer.jsx
+++ client/src/audio-player/AudioPlayer.jsx
@@ -24,10 +24,11 @@ const AudioPlayer = () => {
   const [audioFile, setAudioFile] = useState(0)
   const [audioDuration , setAudioDuration] = useState({minutes: 0 , seconds: 0 })
   const [wavesurfer , setWavesurfer] = useState(null)
+  const [volume, setVolume] = useState(100)
 
   const pause =  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5zm3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5z"/>
   const play = <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"/>
-  const [playButton, setPlayButton] = useState(pause)
+  const [playButton, setPlayButton] = useState(play)
   const waveform = useRef()
   const audioFiles = [audioTest , audioTest1 , audioTest2 , audioTest3]
 
@@ -82,8 +83,10 @@ const AudioPlayer = () => {
   }
 
   function handleVolume(e){
-    let volume = parseFloat(e.target.value / 100) 
-    wavesurfer.setVolume(volume)
+    let vol = parseFloat(e.target.value / 100) 
+    console.log(vol)
+    setVolume(e.target.value)
+    wavesurfer.setVolume(vol)
   }
 
   function handleSeek(e){ 
@@ -94,7 +97,7 @@ const AudioPlayer = () => {
     // Sticky footer
    <div className="audio-player fixed-bottom creme-bg" >
     <Row className="align-items-center">
-      <Col className="ms-3 d-flex align-items-center " >
+      <Col className="ms-3 d-flex align-items-center" >
           <svg onClick={handlePrevious} xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="play-btn bi bi-chevron-bar-left" viewBox="0 0 16 16">
             <path fillRule="evenodd" d="M11.854 3.646a.5.5 0 0 1 0 .708L8.207 8l3.647 3.646a.5.5 0 0 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 0 1 .708 0zM4.5 1a.5.5 0 0 0-.5.5v13a.5.5 0 0 0 1 0v-13a.5.5 0 0 0-.5-.5z"/>
           </svg>
@@ -104,8 +107,10 @@ const AudioPlayer = () => {
           <svg onClick={handleNext} xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="play-btn bi bi-chevron-bar-right" viewBox="0 0 16 16">
             <path fillRule="evenodd" d="M4.146 3.646a.5.5 0 0 0 0 .708L7.793 8l-3.647 3.646a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708 0zM11.5 1a.5.5 0 0 1 .5.5v13a.5.5 0 0 1-1 0v-13a.5.5 0 0 1 .5-.5z"/>
           </svg>
-          <input type="range" id="vol" name="vol" min="0" max="100"  className='align-self-center slider mx-4' onChange={handleVolume}/>
-          <h4 className="text-ltg  ms-auto">{`${audioDuration.minutes}:${audioDuration.seconds}`}</h4>
+          <input type="range" id="vol" name="vol" value={volume} min="0" max="100"  className='align-self-center slider mx-2' onChange={handleVolume}/>
+          <p className="text-primary pe-2 text-va">{volume}</p>
+          
+          <h4 className="text-ltg text-va  ms-auto">{`${audioDuration.minutes}:${audioDuration.seconds}`}</h4>
 
       </Col>
         <Col className="me-4">
