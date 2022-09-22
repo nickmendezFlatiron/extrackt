47d9e332feb4da71f95c5d647bf43b13fe2818c7
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-09-22 07:50:17 -0400

    created table component for editing samples

--- client/src/user-files/EditCollection.jsx
+++ client/src/user-files/EditCollection.jsx
@@ -1,14 +1,22 @@
 import {React , useEffect, useContext}from 'react'
-import { useParams, useNavigate } from 'react-router-dom'
+import { useParams} from 'react-router-dom'
 import CollectionContext from '../context/CollectionContext'
 import { LoginContext } from '../context/LoginContext'
-import { Container } from 'react-bootstrap'
+import EditTable from './EditTable'
+
+import Form from 'react-bootstrap/Form'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Button from 'react-bootstrap/Button'
+import Image from 'react-bootstrap/Image'
+
 const EditCollection = () => {
   const params = useParams()
   const {collection , setCollection} = useContext(CollectionContext)
-  const {user, setErrors, errors} = useContext(LoginContext)
-  const navigate = useNavigate()
+  const {user, setErrors, errors, authenticated, navigate} = useContext(LoginContext)
   
+  const goBack = () => {}
+
   useEffect(()=>{
     setCollection(null)
     if (user) {
@@ -20,7 +28,7 @@ const EditCollection = () => {
             }) 
           } else {
               if (r.status === 401) {
-                navigate("/")
+               return navigate
               }
               r.json().then(e => {
                 setErrors(e.errors)
@@ -29,10 +37,34 @@ const EditCollection = () => {
         })
     }
   },[user])
+  if (authenticated === false) return navigate
+  const created = collection?.created_at.split("T")[0]
+
   return (
-    <Container>
-      Hello WOrld
-    </Container>
+    
+      <div className="mb-3 pb-5 mx-4">
+        <Row>
+          <Col className="height-match rounded-3 p-2" lg={4} xs={4}> 
+            <Form>
+              <Col className="pb-3 d-flex">
+              <Button className="me-4" variant="link" onClick={() => goBack()}> {`< Back`} </Button>
+              <Button type="submit" className="px-4 fs-5">Apply</Button>
+              </Col> 
+              <Col >
+                <Image src={collection?.cover_art_url} className="cover-preview offset"/>
+                <h3 className="ms-5 text-black">{collection?.samples.length} Sample(s)</h3>
+              </Col>
+                <h2 className='text-black fw-bold '>{collection?.name}</h2>
+                <h2 className="text-black">{collection?.user.username}</h2>
+                <p className="text-black">Released: {created}</p>
+            </Form>
+          </Col>
+          <Col>
+            <EditTable />
+          </Col>
+        </Row>
+      </div>
+    
   )
 }
 
